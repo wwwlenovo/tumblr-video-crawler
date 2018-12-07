01f1980d82577c182025e2a32420c2bb94f61305
Guo Chen <guo.chen@sap.com>
2018-12-07 17:45:16 +0800

    fix bug

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const puppeteer = require('puppeteer');
 const fs = require('fs-extra');
 const child_process = require('child_process');
 const FOLLOW = 'https://www.tumblr.com/following/';
-const DOWNLOAD_CONCURRENCY = 5;
+const LIKES = 'https://www.tumblr.com/likes';
+const DOWNLOAD_CONCURRENCY = 8;
 const CRAWL_CONCURRENCY = 3;
-const MAX_PAGE = 10;
+const MAX_PAGE = 20;
 
 (async () => {
     const browser = await puppeteer.launch({
         headless: true
     });
     const page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(90000)
+    await page.setDefaultNavigationTimeout(90000);
     await page.goto('https://www.tumblr.com/login');
     await page.type('#signup_determine_email', '@live.com');
     await page.click('#signup_forms_submit > span.signup_determine_btn.active');
@@ -33,9 +34,11 @@ const MAX_PAGE = 10;
             return elems.map(elem => elem.href);
         }));
     }
+    userList.push(LIKES); //download likes in www folder
     console.log(userList);
     for (let i = 1; i <= userList.length; i = i + CRAWL_CONCURRENCY) {
         try {
+            console.log(`${i}/${userList.length}`);
             await crawlVideo(browser, userList.filter((value, index) => {
                 return index >= i - 1 && index < i + CRAWL_CONCURRENCY - 1;
             }));
@@ -50,9 +53,15 @@ const MAX_PAGE = 10;
 async function download(urls, folder = 'video') {
     let promises = await urls.map(async url => {
         try {
-            await fs.ensureDir(`./t/${folder}`);
+            const path = `d:/t/${folder}`;
+            await fs.ensureDir(path);
+            const parsedUrl = parseUrl(url);
             //console.log(`cd ./t/${ folder } && curl - C - O ${ parseUrl(url) }`)
-            await child_process.exec(`cd ./t/${folder} && curl -C -O ${parseUrl(url)}`);
+            if(!fs.existsSync(`${path}/${parsedUrl.slice(parsedUrl.lastIndexOf('/') + 1)}`)) {
+                await child_process.exec(`d: && cd ./t/${folder} && curl -O ${parsedUrl}`);
+            } else {
+                console.log(`${path}/${parsedUrl.slice(parsedUrl.lastIndexOf('/') + 1)} exisit.`);
+            }
         } catch (err) {
             console.error(err)
         }
@@ -76,6 +85,7 @@ async function crawlVideo(browser, userArray) {
 async function gerUrlPerUser(browser, user) {
     console.log(`${user} begin to crawl url`)
     const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(90000);
     try {
         await page.goto(user);
     } catch (error) {
@@ -85,10 +95,11 @@ async function gerUrlPerUser(browser, user) {
                 user = 'http' + user.slice(5);
                 console.log(`Try ${user}`);
                 await page.goto(user);
-            } else {
-                await page.close();
-                return await ('Closed');
-            }
+            }else if (user.startsWith('http')) {
+                user = 'https' + user.slice(4);
+                console.log(`Try ${user}`);
+                await page.goto(user);
+            } 
         } catch (error) {
             console.log(error)
             await page.close();
